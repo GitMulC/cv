77a9b7cae636faa13b45641f844970d14ca61097
GitMulC <cmulvo@gmail.com>
2022-09-12 19:39:27 +0000

    Add JS function to show/hide elements

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Global Variables
+ */
+let startUp = document.getElementById('first-div')
+let cvArea = document.getElementById('second-div')
+
+function showStuff() {
+    startUp.classList.add('hidden')
+    cvArea.classList.remove('hidden')
+}
