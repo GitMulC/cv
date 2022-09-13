a2eb5208c27f5786ed7747dfc7b6869edf0cfaf1
GitMulC <cmulvo@gmail.com>
2022-09-13 22:34:36 +0000

    Comment out irrelevant logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let startUp = document.getElementById('first-div')
 let cvArea = document.getElementById('second-div')
 let backImg = document.getElementById('back-img')
 
-document.getElementById("first-div").onclick = function () {
-    startUp.classList.add('hidden');
-    backImg.classList.toggle("fade");
-  };
+// document.getElementById("first-div").onclick = function () {
+//     startUp.classList.add('hidden');
+//     backImg.classList.toggle("fade");
+//   };
